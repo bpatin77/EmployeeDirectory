db9e8680c6641884c0a8aa0fadb23c896ef8540c
Blake <bpatin77@gmail.com>
2024-10-07 12:37:30 -0500

    Finished steps 1 and 2

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 // TODO: this file :)
+const express = require("express");
+const employess = require("./employess");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello employees!");
+});
+
+app.get("/employees", (req, res) => {
+  res.json(employess);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port #${PORT}`);
+});
